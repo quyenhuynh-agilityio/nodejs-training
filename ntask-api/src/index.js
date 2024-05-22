86cf398e480545a7e6bd0c7f6c6f78166123edba
Quyen Huynh <quyen.huynh@asnet.com.vn>
2024-05-22 16:56:28 +0700

    feat: sync database and start server

--- ntask-api/src/index.js
+++ ntask-api/src/index.js
@@ -1,9 +1,23 @@
 import express from "express";
 import { nTaskRoute } from "./routes/index.js";
 import { TaskRoute } from "./routes/task.js";
+import { db } from "./db.js";
 
 const PORT = 3000;
 const app = express();
+
+// Sync database and start server
+db()
+  .sequelize.sync({ alter: true })
+  .then(() => {
+    console.log("Database synced");
+    app.listen(8000, () => {
+      console.log(`Server is running on port ${8000}`);
+    });
+  })
+  .catch((err) => {
+    console.error("Unable to sync the database:", err);
+  });
 
 // Return as formatted and tabbed JSON output
 app.set("json spaces", 4);
